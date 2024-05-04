85f390004348b166e498dfc30eeb72487cab1e54
Luo <Luo@DESKTOP-O3LV72F>
2024-05-04 16:26:51 +0800

    perf:简化了 Properties.Settings.Default 的使用

--- SeerRandomSkin/FormConfig.cs
+++ SeerRandomSkin/FormConfig.cs
@@ -13,6 +13,8 @@ namespace SeerRandomSkin
 {
     public partial class FormConfig : Form
     {
+        private Properties.Settings SettingsDef = Properties.Settings.Default;
+
         public FormConfig()
         {
             InitializeComponent();
@@ -21,52 +23,52 @@ namespace SeerRandomSkin
         private void FormConfig_Load(object sender, EventArgs e)
         {
             // 初始化配置项
-            checkBox_RandomSkin.Checked = Properties.Settings.Default.IsRandomSkin;
-            checkBox_h5_first.Checked = Properties.Settings.Default.IsH5First;
-            numericUpDown_skinCeiling.Value = Properties.Settings.Default.SkinRangeCeiling;
-            numericUpDown_skinFloor.Value = Properties.Settings.Default.SkinRangeFloor;
-            richTextBox_SkinBlackList.Text = Properties.Settings.Default.SkinBlackList;
-            comboBox_font.Text = Properties.Settings.Default.BrowserFont;
-            numericUpDown_win_width.Value = Properties.Settings.Default.WinWidth;
-            numericUpDown_win_height.Value = Properties.Settings.Default.WinHeight;
-            checkBox_resource_ad_panel.Checked = Properties.Settings.Default.IsChangeAdPanel;
-            checkBox_resource_background.Checked = Properties.Settings.Default.IsChangeBackground;
-            checkBox_resource_vip_icon.Checked = Properties.Settings.Default.IsChangeVipIcon;
-            checkBox_resource_bg_h5.Checked = Properties.Settings.Default.IsChangeH5LoginBg2024;
+            checkBox_RandomSkin.Checked = SettingsDef.IsRandomSkin;
+            checkBox_h5_first.Checked = SettingsDef.IsH5First;
+            numericUpDown_skinCeiling.Value = SettingsDef.SkinRangeCeiling;
+            numericUpDown_skinFloor.Value = SettingsDef.SkinRangeFloor;
+            richTextBox_SkinBlackList.Text = SettingsDef.SkinBlackList;
+            comboBox_font.Text = SettingsDef.BrowserFont;
+            numericUpDown_win_width.Value = SettingsDef.WinWidth;
+            numericUpDown_win_height.Value = SettingsDef.WinHeight;
+            checkBox_resource_ad_panel.Checked = SettingsDef.IsChangeAdPanel;
+            checkBox_resource_background.Checked = SettingsDef.IsChangeBackground;
+            checkBox_resource_vip_icon.Checked = SettingsDef.IsChangeVipIcon;
+            checkBox_resource_bg_h5.Checked = SettingsDef.IsChangeH5LoginBg2024;
             // 遍历系统字体
             foreach (var f in FontFamily.Families)
             {
                 comboBox_font.Items.Add(f.Name);
             }
-            textBox1.Text = Properties.Settings.Default.AutoExecuteSoftwarePath1;
+            textBox1.Text = SettingsDef.AutoExecuteSoftwarePath1;
         }
 
         private void button_Save_Click(object sender, EventArgs e)
         {
-            Properties.Settings.Default.IsRandomSkin = checkBox_RandomSkin.Checked;
-            Properties.Settings.Default.IsH5First = checkBox_h5_first.Checked;
-            Properties.Settings.Default.WinWidth = numericUpDown_win_width.Value;
-            Properties.Settings.Default.WinHeight = numericUpDown_win_height.Value;
-            Properties.Settings.Default.SkinRangeCeiling = (int)numericUpDown_skinCeiling.Value;
-            Properties.Settings.Default.SkinRangeFloor = (int)numericUpDown_skinFloor.Value;
-            Properties.Settings.Default.IsChangeAdPanel = checkBox_resource_ad_panel.Checked;
-            Properties.Settings.Default.IsChangeBackground = checkBox_resource_background.Checked;
-            Properties.Settings.Default.IsChangeVipIcon = checkBox_resource_vip_icon.Checked;
-            Properties.Settings.Default.IsChangeH5LoginBg2024 = checkBox_resource_bg_h5.Checked;
+            SettingsDef.IsRandomSkin = checkBox_RandomSkin.Checked;
+            SettingsDef.IsH5First = checkBox_h5_first.Checked;
+            SettingsDef.WinWidth = numericUpDown_win_width.Value;
+            SettingsDef.WinHeight = numericUpDown_win_height.Value;
+            SettingsDef.SkinRangeCeiling = (int)numericUpDown_skinCeiling.Value;
+            SettingsDef.SkinRangeFloor = (int)numericUpDown_skinFloor.Value;
+            SettingsDef.IsChangeAdPanel = checkBox_resource_ad_panel.Checked;
+            SettingsDef.IsChangeBackground = checkBox_resource_background.Checked;
+            SettingsDef.IsChangeVipIcon = checkBox_resource_vip_icon.Checked;
+            SettingsDef.IsChangeH5LoginBg2024 = checkBox_resource_bg_h5.Checked;
             // 检查黑名单字符串格式是否正确
             string pattern = "^[0-9,]*$";
             var m = Regex.Match(richTextBox_SkinBlackList.Text, pattern);
             if(m.Success)
             {
-                Properties.Settings.Default.SkinBlackList = richTextBox_SkinBlackList.Text;
+                SettingsDef.SkinBlackList = richTextBox_SkinBlackList.Text;
             }
             else
             {
                 MessageBox.Show("皮肤黑名单格式错误");
             }
-            Properties.Settings.Default.BrowserFont = comboBox_font.Text;
-            Properties.Settings.Default.AutoExecuteSoftwarePath1 = textBox1.Text;
-            Properties.Settings.Default.Save();
+            SettingsDef.BrowserFont = comboBox_font.Text;
+            SettingsDef.AutoExecuteSoftwarePath1 = textBox1.Text;
+            SettingsDef.Save();
         }
 
         private void button1_Click(object sender, EventArgs e)
