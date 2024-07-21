c14f0fa1e72710599baa265a0148545ebefee524
VM-Win <Administrator@WIN-NNNCRSJCEOU>
2024-07-21 11:12:00 +0800

    perf: 缩小截图

--- SeerRandomSkin/FormScreenShot.cs
+++ SeerRandomSkin/FormScreenShot.cs
@@ -28,20 +28,27 @@ namespace SeerRandomSkin
         public void ScreenShot()
         {
             // 获取屏幕宽度和高度
-            Width = MainForm.Size.Width / 2;
-            Height = MainForm.Size.Height - titleAndMenuHeight;
+            int screenWidth = MainForm.Size.Width / 2;
+            int screenHeight = MainForm.Size.Height - titleAndMenuHeight;
 
             // 创建一个屏幕图像对象
-            using (Bitmap bitmap = new Bitmap(Width, Height))
+            using (Bitmap bitmap = new Bitmap(screenWidth, screenHeight))
             {
                 using (Graphics graphics = Graphics.FromImage(bitmap))
                 {
                     // 拷贝屏幕图像到位图中
-                    graphics.CopyFromScreen(MainForm.Location.X + Width, MainForm.Location.Y + titleAndMenuHeight, 0, 0, new System.Drawing.Size(Width, Height));
+                    graphics.CopyFromScreen(MainForm.Location.X + screenWidth, MainForm.Location.Y + titleAndMenuHeight + 60, 0, 0, new System.Drawing.Size(screenWidth, screenHeight));
 
-                    // 在本窗口中显示
-                    pictureBox1.Dock = DockStyle.Fill;
-                    pictureBox1.Image = Image.FromHbitmap(bitmap.GetHbitmap());
+                    // 缩小图片
+                    using (var smallPic = new Bitmap(bitmap, bitmap.Width / 2, bitmap.Height / 2))
+                    {
+                        // 在本窗口中显示
+                        pictureBox1.Dock = DockStyle.Fill;
+                        pictureBox1.Image = Image.FromHbitmap(smallPic.GetHbitmap());
+                        // 更新窗口大小
+                        Width = smallPic.Width;
+                        Height = smallPic.Height;
+                    }
                 }
             }
         }
