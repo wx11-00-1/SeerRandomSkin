248ca2fcf748d9911d6cd3ccb6b85547db98c5ee
zhihong luo <1754256138@qq.com>
2025-02-21 20:20:03 +0800

    fix: 内置脚本

--- SeerRandomSkin/FormFlashFightHandler.cs
+++ SeerRandomSkin/FormFlashFightHandler.cs
@@ -332,6 +332,7 @@ WxSc._in = () => {
         private void FormFlashFightHandler_Load(object sender, EventArgs e)
         {
             ResetLvTemplate();
+            richTextBox_script.Text = Properties.Settings.Default.FlashFightTemplate;
         }
 
         private static string JsToAsync(string js)
